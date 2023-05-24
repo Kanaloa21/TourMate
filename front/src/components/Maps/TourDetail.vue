8a964465571bc039dbb4bccbaf2efa044d7db030
Kanaloa <kjwkjw1104@gmail.com>
2023-05-24 21:46:28 +0900

    add Detail side bar

---
+++ front/src/components/Maps/TourDetail.vue
@@ -0,0 +1,111 @@
+<template>
+  <div
+    class="px-4 list bb scroll"
+    style="overflow-y: scroll"
+    @scroll="handleNotificationListScroll"
+  >
+    <div><h1>title</h1></div>
+    <hr />
+    <div>내용입력</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TourDetail",
+  components: {},
+  data() {
+    return {};
+  },
+  created() {},
+  methods: {},
+
+  computed: {},
+};
+</script>
+
+<style scoped>
+hr {
+  height: 3px;
+  background-color: rgb(200, 200, 200);
+  border-width: 2px;
+  border-radius: 15px;
+}
+.list {
+  /* position: fixed; */
+  top: 0vh;
+  bottom: 0;
+  /* left: 80px; */
+  right: 0vw;
+  padding: 0px 0 0;
+  box-shadow: 0 2px 5px 0 rgb(0 0 0 / 25%), 0 2px 10px 0 rgb(0 0 0 / 25%);
+  /* 최소 창 크기 500px (크롬기준) */
+  width: 350px;
+  height: 100vh;
+  z-index: 500;
+  background-color: rgb(133, 133, 132);
+}
+
+.button {
+  border-width: 2px;
+  border-radius: 15px;
+  transition: background-color 0.5s ease-out 10ms;
+  background-color: #42b983;
+}
+.dropdown {
+  width: 160px;
+  border-width: 2px;
+  border-radius: 15px;
+  transition: background-color 0.5s ease-out 10ms;
+  background-color: #42b983;
+}
+/* 
+.button:hover {
+  background-color: rgb(133, 133, 132);
+  border-width: 2px;
+} */
+
+.list-item {
+  font-family: Jeju Gothic;
+}
+
+.sidebar-toggle {
+  top: 20vh;
+  left: 420px;
+  padding: 100px 0 0;
+  box-shadow: 0 2px 5px 0 rgb(0 0 0 / 5%), 0 2px 10px 0 rgb(0 0 0 / 5%);
+  width: 30px;
+  height: 70px;
+  padding: 0px;
+  background-color: rgb(59, 59, 59);
+}
+
+.bb {
+  --list-load-time: 300ms;
+}
+
+.side-list {
+  animation: list-load var(--list-load-time) ease-in;
+}
+
+@keyframes list-load {
+  0% {
+    transform: translateX(-100%);
+  }
+  100% {
+    transform: translateX(0%);
+  }
+}
+
+.scroll::-webkit-scrollbar {
+  width: 12px;
+}
+
+.scroll::-webkit-scrollbar-track {
+  background: #ddd;
+}
+
+.scroll::-webkit-scrollbar-thumb {
+  background: #42b983;
+}
+</style>
--- front/src/views/MapsView.vue
+++ front/src/views/MapsView.vue
@@ -4,6 +4,7 @@
     <div class="position-relative bb">
       <background-map class="position-absolute anime-map" />
       <tour-list class="position-absolute side-list" style="z-index: 2" />
+      <tour-detail class="position-absolute side-list-reverse" style="z-index: 3"></tour-detail>
     </div>
   </div>
 </template>
@@ -12,10 +13,11 @@
 import SidebarNav from "@/components/SidebarNav.vue";
 import BackgroundMap from "@/components/Maps/BackgroundMap.vue";
 import TourList from "@/components/Maps/TourList.vue";
+import TourDetail from "@/components/Maps/TourDetail.vue";
 
 export default {
   name: "MapsView",
-  components: { BackgroundMap, SidebarNav, TourList },
+  components: { BackgroundMap, SidebarNav, TourList, TourDetail },
   data() {
     return {
       message: "",
@@ -42,6 +44,21 @@ h1 {
 @keyframes list-load {
   0% {
     transform: translateX(-150%);
+    opacity: 0;
+  }
+  100% {
+    transform: translateX(0%);
+    opacity: 1;
+  }
+}
+
+.side-list-reverse {
+  animation: list-load-reverse var(--list-load-time) cubic-bezier(0.13, 0.63, 0.62, 1.37);
+}
+
+@keyframes list-load-reverse {
+  0% {
+    transform: translateX(150%);
     opacity: 0;
   }
   100% {
