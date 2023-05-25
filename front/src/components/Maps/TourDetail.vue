1d1c4f299f8865aaf85a4f7f1a995595bcd743ad
kjwkjw1104 <kjwkjw1104@gmail.com>
2023-05-25 17:09:58 +0900

    edit TourDetail

--- front/src/components/Maps/TourDetail.vue
+++ front/src/components/Maps/TourDetail.vue
@@ -5,12 +5,12 @@
     style="overflow-y: scroll"
     @scroll="handleNotificationListScroll"
   >
-    <div><b-img class="mt-2 rounded" :src="attractionDetail.imageURL" width="280px"></b-img></div>
+    <div><b-img class="mt-2 rounded" :src="attractionDetail.imageURL" width="260px"></b-img></div>
     <div class="mt-4 font">
-      <h1>{{ attractionDetail.title }}</h1>
+      <h3>{{ attractionDetail.title }}</h3>
     </div>
-    <hr />
     <div>{{ attractionDetail.addr1 }}</div>
+    <hr />
     <b-row class="mt-3">
       <b-col
         ><b-button variant="success">평가 {{ attractionComments.length }}</b-button></b-col
@@ -19,16 +19,15 @@
         ><b-button variant="danger">좋아요 {{ attractionDetail.likeCount }}</b-button></b-col
       >
     </b-row>
-    <div class="p-2 mt-3 bg-light rounded">
+    <div class="p-2 mt-3 bg-light rounded rowb">
       <b-input-group v-if="userId" class="mt-3 mb-3">
-        <b-form-input placeholder="한줄평을 남겨주세요!" v-model="comment"></b-form-input>
+        <b-form-input
+          placeholder="한줄평을 남겨주세요!"
+          v-model="comment"
+          @keypress.enter="writeAttractionComment"
+        ></b-form-input>
         <b-input-group-append>
-          <b-button
-            variant="info"
-            @keypress.enter="writeAttractionComment"
-            @click="writeAttractionComment"
-            >작성</b-button
-          >
+          <b-button variant="info" @click="writeAttractionComment">작성</b-button>
         </b-input-group-append>
       </b-input-group>
       <tour-comment-item v-for="item in commentList" :key="item.id" v-bind="item" />
@@ -125,6 +124,9 @@ hr {
   border-bottom-left-radius: 30px;
 }
 
+.rowb {
+  background-color: rgb(200, 200, 200);
+}
 .button {
   border-width: 2px;
   border-radius: 15px;
